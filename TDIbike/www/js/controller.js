61b2e843926007cf1e877b6884dc3e43e5e3a7a8
Kyria Márquez <kyriamar@gmail.com>
2015-09-28 00:59:20 -0300

    distance function
    distance function

--- TDIbike/www/js/controller.js
+++ TDIbike/www/js/controller.js
@@ -11,6 +11,29 @@ angular.module('starter', ['ionic', 'uiGmapgoogle-maps', 'ngCordova'])
 		zoom: 13, 
 	};
 
+	function toRad(value) {
+	  var RADIANT_CONSTANT = 0.0174532925199433;
+	  return (value * RADIANT_CONSTANT);
+	};
+
+	function calculateDistance(starting, ending) {
+	  var KM_RATIO = 6371; // Radius of the earth in km
+	  try {      
+	    var dLat = toRad(ending.latitude - starting.latitude);
+	    var dLon = toRad(ending.longitude - starting.longitude);
+	    var lat1Rad = toRad(starting.latitude);
+	    var lat2Rad = toRad(ending.latitude);
+	    
+	    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+	            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
+	    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    var d = KM_RATIO * c; // Distance in km
+	    return d;
+	  } catch(e) {
+	    return -1;
+	  }
+	}
+
 	var posOptions = {timeout: 10000, enableHighAccuracy: false};
     $cordovaGeolocation
     	.getCurrentPosition(posOptions).then(function (position) {
